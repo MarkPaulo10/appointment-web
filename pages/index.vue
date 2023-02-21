5fc0716df6529c756c3317e79bcca7e666ecd265
mcruz12 <mcruz@centralizedinc.com>
2023-02-21 16:35:41 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <a-card title="Hello World" style="width: 300px">
+      Welcome to my nuxt app!
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #f2f2f2;
+}
+</style>
